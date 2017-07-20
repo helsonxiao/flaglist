5afd12b9c3f5d0903acedc9706cdc45e3356eaab
helsonxiao <helsonxiao@gmail.com>
2017-07-20 18:53:22 +0800

    new serializers for REST

---
+++ events/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Event
+
+
+class EventSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Event
+        fields = ('owner', 'title', 'text', 'created_date', 'status')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    events = serializers.HyperlinkedRelatedField(many=True, view_name='event-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'events')
