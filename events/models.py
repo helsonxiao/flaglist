22cf56dff3df209e789453c10be593d774c9994c
helsonxiao <helsonxiao@gmail.com>
2017-07-20 18:54:37 +0800

    change user -> owner, add related_name for API query

--- events/models.py
+++ events/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class Event(models.Model):
-    user = models.ForeignKey('auth.User')  # todo
+    owner = models.ForeignKey('auth.User', related_name='events')
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
