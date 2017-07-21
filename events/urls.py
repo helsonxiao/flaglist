68f70ea6ebd5268be42d01b1a364ef288e235942
helsonxiao <helsonxiao@gmail.com>
2017-07-21 13:21:34 +0800

    use DefaultRouter to control url automatically

--- events/urls.py
+++ events/urls.py
@@ -1,16 +1,24 @@
 from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
+router = DefaultRouter()
+router.register(r'events', views.EventViewSet)
+router.register(r'users', views.UserViewSet)
+
 urlpatterns = [
     # API View
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/$', views.api_root),
-    url(r'^api/events/$', views.EventList.as_view(), name='event-list'),
-    url(r'^api/events/(?P<pk>[0-9]+)/$', views.EventDetail.as_view(), name='event-detail'),
-    url(r'^api/users/$', views.UserList.as_view(), name='user-list'),
-    url(r'^api/users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
+
+    # url(r'^api/$', views.api_root),
+    # url(r'^api/events/$', views.EventList.as_view(), name='event-list'),
+    # url(r'^api/events/(?P<pk>[0-9]+)/$', views.EventDetail.as_view(), name='event-detail'),
+    # url(r'^api/users/$', views.UserList.as_view(), name='user-list'),
+    # url(r'^api/users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
+
+    url(r'^api/', include(router.urls)),
 
     # Normal View
     url(r'^$', views.index, name='index'),
@@ -27,4 +35,4 @@ urlpatterns = [
     url(r'^(?P<event_id>[0-9]+)/finish/$', views.finish_event, name='finish_event'),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
