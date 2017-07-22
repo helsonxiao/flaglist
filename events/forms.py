ecc74e15e8f01f4b80d1851d06cc0e6d476cf966
helsonxiao <helsonxiao@gmail.com>
2017-07-22 14:29:13 +0800

    fix priority setting error, add expire_date format & initial

--- events/forms.py
+++ events/forms.py
@@ -1,8 +1,14 @@
+from todolist import settings
 from .models import Event
 from django import forms
+from .models import PRIORITY_CHOICES
 
 
-class EventForm(forms.ModelForm):  # todo
+class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ('title', 'text', 'expire_date', 'priority')
+        fields = ('title', 'text',)
+
+    expire_date = forms.DateField(widget=forms.DateInput,
+                                  input_formats=settings.DATE_INPUT_FORMATS, initial='yyyy-mm-dd')
+    priority = forms.ChoiceField(choices=PRIORITY_CHOICES)
